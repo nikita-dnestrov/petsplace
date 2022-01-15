c33345c36c79db90b37334b2a98a88e2c0e4aed1
trueubermensch <nikita.dnestrov@gmail.com>
2022-01-15 15:02:06 +0200

    Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = process.env.PORT || 5000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT, () => console.log(`Up on ${PORT}!`));
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
